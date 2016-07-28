d0d52b03d01098d280e196779ac93a1c1343871f
Ricardo Nunez <rnunez@mytrus.com>
2016-07-28 12:33:58 -0700

    Adds Carthage support

---
+++ PDFReader/PDFReader.h
@@ -0,0 +1,19 @@
+//
+//  PDFReader.h
+//  PDFReader
+//
+//  Created by Ricardo Nunez on 7/28/16.
+//  Copyright © 2016 mytrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDFReader.
+FOUNDATION_EXPORT double PDFReaderVersionNumber;
+
+//! Project version string for PDFReader.
+FOUNDATION_EXPORT const unsigned char PDFReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDFReader/PublicHeader.h>
+
+
