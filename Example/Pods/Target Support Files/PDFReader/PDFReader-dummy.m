cd8037ada7a876fdba7fdd3da519310638501e5d
Ricardo Nunez <rnunez@mytrus.com>
2016-10-10 00:54:02 -0700

    Cleans up demo project structure

--- Example/Pods/Target Support Files/PDFReader/PDFReader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PDFReader : NSObject
-@end
-@implementation PodsDummy_PDFReader
-@end
--- Example/Pods/Target Support Files/Pods-PDFReader_Example/Pods-PDFReader_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PDFReader_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PDFReader_Example
-@end
